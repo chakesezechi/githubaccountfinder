118c6d111b51a9b51c467b3b3a32e6d8d5870a63
Chekwube Ezechi <zechirayjr@yahoo.com>
2021-07-21 16:58:25 +0100

    added add event listener function to the input

--- app.js
+++ app.js
@@ -10,9 +10,17 @@ searchUser.addEventListener('keyup', (e) => {
     const userText = e.target.value;
 
     if(userText !== ''){
+      // Make http call
       github.getUser(userText)
       .then(data => {
-        console.log(data)
+        if(data.profile.message === 'Not Found') {
+          // Show alert
+
+        } else {
+          // Show profile
+        }
       })
+    } else {
+      // Clear profile
     }
 });
