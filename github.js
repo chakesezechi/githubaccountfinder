3427d2ed8b733529b480dfb1780957a93f2555df
Chekwube Ezechi <zechirayjr@yahoo.com>
2021-07-22 12:10:00 +0100

    added async await to fetch the repo

--- github.js
+++ github.js
@@ -2,16 +2,21 @@ class GitHub {
   constructor(){
     this.client_id = '40dc98ece91bdc8acba7';
     this.client_secret = '6db503e476ada9ed6b26a3fc7167d263f543c20e';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc'
   }
 
   async getUser(user){
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-    const profile = await profileResponse.json()
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const profile = await profileResponse.json()
+    const repos = await repoResponse.json()
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
